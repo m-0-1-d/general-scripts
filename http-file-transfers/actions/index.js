8958b8d3f38815f1dfe893a00fa706e56b0f1b52
m01d <blablablaaaa777@gmail.com>
2025-04-13 21:57:48 +0200

    updated http-file-transfers

--- http-file-transfers/actions/index.js
+++ http-file-transfers/actions/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { uploadWithForm } = require("./uploadWithForm.js");
 const { uploadWithBase64 } = require("./uploadWithBase64.js");
 const { uploadWithDataBinary } = require("./uploadWithDataBinary.js");
--- http-file-transfers/libs/readRequestBody.js
+++ http-file-transfers/libs/readRequestBody.js
@@ -1,3 +1,5 @@
+"use strict";
+
 exports.readRequestBody = async (req) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
